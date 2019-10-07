1c203e5833150ee0525a43b4785ffc7ad85a9d52
DIY-Machines <42316159+DIY-Machines@users.noreply.github.com>
2019-10-07 14:00:56 +0100

    Update DIYM_Dry_Ice_V3.ino

--- DIYM_Dry_Ice_V3.ino
+++ DIYM_Dry_Ice_V3.ino
@@ -5,7 +5,7 @@ This is an easy to build dry ice fog machine. It's controlled by an Arduino Nano
 
 ==========
 
-More info: https://diymachines.co.uk/
+More info: https://diymachines.co.uk/projects/bluetooth-controlled-dry-ice-machine.html
 
 3D printed parts can be downloaded from here: https://www.thingiverse.com/thing:
 
